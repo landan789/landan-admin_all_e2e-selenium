476a9682170a8ee16850d1270a9c1133c0ddc656
Landan Hsu <doroke7@gmail.com>
2018-12-05 00:34:56 +0800

    thread

--- test/thread.py
+++ test/thread.py
@@ -9,10 +9,12 @@ def worker(num):
     thread worker function
     :return:
     """
-    time.sleep(1)
+    time.sleep(0.01)
     print("The num is  %d" % num)
     return
  
 for i in range(20):
     t = threading.Thread(target=worker, args=(i,), name="t.%d" % i)
+    name = t.getName()
+    print("Thread %s starts" % name)
     t.start()
